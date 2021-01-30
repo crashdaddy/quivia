9b7d4923157fbbf591648585db9276f870db0be0
crashdaddy <kennethscoggins@mail.com>
2021-01-30 17:44:42 -0600

    connecting to API

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,33 @@
+import React, { Component } from 'react';
 import '../../App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        questions: []
+    };
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  getQuestions = () => {
+ 
+    let url = "https://opentdb.com/api.php?amount=1&type=multiple";
+    fetch(url)
+    .then(res=>res.json())
+    .then(data => {
+      
+      this.setState({
+        questions: data.results
+      })
+    })
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         
@@ -9,11 +35,21 @@ function App() {
           Brainium
         </p>
         
-        <span style={{color:"blue"}}>Wrinkle Your Brain</span>
-        
+        <span style={{color:"blue"}}>Wrinkle Your Brain</span><p/>
+        {this.state.questions && this.state.questions.map( question => 
+          <div>
+            <div dangerouslySetInnerHTML={{ __html: question.question }}></div><p/>
+            {question.incorrect_answers.map(wrong => 
+              <div>
+                {wrong}
+              </div>
+              )}
+          </div>
+          )}
       </header>
     </div>
   );
+}
 }
 
 export default App;
