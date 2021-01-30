70ddfba85bb87c411a5dcbd5e5ef0f425db7dec2
crashdaddy <kennethscoggins@mail.com>
2021-01-30 16:45:40 -0600

    getting rid of template stuff

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,16 @@
-import logo from '../../logo.svg';
 import '../../App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Brainium
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <span style={{color:"blue"}}>Feel yourself getting smarter</span>
+        
       </header>
     </div>
   );
