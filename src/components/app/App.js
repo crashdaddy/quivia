4f24e4399707a58e410e6c462d2a169efeb766cd
crashdaddy <kennethscoggins@mail.com>
2021-01-30 19:06:45 -0600

    can tell right from wrong answers

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     super(props);
     this.state = {
         questions: [],
-        answers: []
+        answers: [],
+        correct: false
     };
   }
 
@@ -28,7 +29,7 @@ getRandomInt = (min, max) => {
     fetch(url)
     .then(res=>res.json())
     .then(data => {
-        let rightPosition = this.getRandomInt(0,2)
+     let rightPosition = this.getRandomInt(0,2)
      let answerArray = data.results[0].incorrect_answers
      answerArray.splice(rightPosition,0,data.results[0].correct_answer)
       console.log(answerArray)
@@ -39,6 +40,20 @@ getRandomInt = (min, max) => {
    
     
     })
+  }
+
+  checkAnswer(answer) {
+    if (answer===this.state.questions[0].correct_answer) {
+      console.log("correct") 
+      this.setState({
+        correct:true
+      })
+    } else {
+      console.log("wrong");
+      this.setState({
+        correct:false
+      })
+  }
   }
 
   render() {
@@ -52,14 +67,21 @@ getRandomInt = (min, max) => {
         
         <span style={{color:"blue"}}>Wrinkle Your Brain</span><p/>
         {this.state.questions && this.state.questions.map( question => 
-          <div>
+          <div style={{width:"100%"}}>
             <div dangerouslySetInnerHTML={{ __html: question.question }}></div><p/>
-            {this.state.answers.map(wrong => 
-              <div dangerouslySetInnerHTML={{ __html: wrong }}>
+            <div style={{textAlign:"center",display:"flex",flexDirection:"row",justifyContent:"space-around",alignItems:"center",alignContent:"center"}}>
+            {this.state.answers.map(answer => 
+              <div style={{padding:"20px",width:"20%",margin:"20px",backgroundColor:"blue"}} onClick={()=>this.checkAnswer(answer)} dangerouslySetInnerHTML={{ __html: answer }}>
               </div>
               )}
+            </div>
           </div>
           )}
+          {this.state.correct && 
+          <div>
+            You got it!
+          </div>
+          }
       </header>
     </div>
   );
